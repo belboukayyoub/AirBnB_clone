24f7ebd0756ffeedcb722caac37e465e92fa7c21
Belbouk Ayyoub <belboukayyoub@gmail.com>
2023-10-14 11:23:32 +0100

    0x00. Add base model and tests forlder

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,38 @@
 #!/usr/bin/python3
+"""module defines a base class for all other classes to inherit from"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """Class for all other classes to inherit from."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a BaseModel instance."""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def save(self):
+        """Update the public instance attribute
+        updated_at with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Return a dictionary containing
+        all keys/values of __dict__ of the instance.
+
+        Returns:
+            dict: A dictionary representation of the object.
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        obj_dict["__class__"] = self.__class__.__name__
+        return obj_dict
+
+    def __str__(self):
+        """Return a string representation of the object."""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self
+                                     .id, self.__dict__)
