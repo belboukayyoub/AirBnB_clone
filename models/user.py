6ad78a8f4af987899f14566bf753528936fd6c3c
Belbouk Ayyoub <belboukayyoub@gmail.com>
2023-10-16 04:08:57 +0100

    0x00- Task 8. First User

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module defines User class"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """User Class"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
