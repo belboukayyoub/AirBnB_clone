6492c41d23574d703b37f1e0864ca2a9f658c726
Belbouk Ayyoub <belboukayyoub@gmail.com>
2023-10-16 03:36:35 +0100

    0x00- Task 7. create

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 Command interpreter for AirBnB clone project
 """
 import cmd
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -19,6 +20,17 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Called when an empty line is entered."""
         pass  # Do nothing on an empty line
+
+    def do_create(self, line):
+        """Creates an instance."""
+        if line == "" or line is None:
+            print("** class name missing **")
+        elif line not in storage.classes():
+            print("** class doesn't exist **")
+        else:
+            b = storage.classes()[line]()
+            b.save()
+            print(b.id)
 
 
 if __name__ == "__main__":
