466fa8127737e9af87081f1d7ffbc50a8f277de7
Belbouk Ayyoub <belboukayyoub@gmail.com>
2023-10-16 02:54:32 +0100

    0x00- Task 6. Console 0.0.1

--- console.py
+++ console.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self):
+        """quit the program
+
+        Returns:
+            bool: ture to quite
+        """
+        return True
+
+    def do_EOF(self, line):
+        "Exit"
+        return True
+
+    def emptyline(self):
+        pass  # Do nothing on an empty line
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
