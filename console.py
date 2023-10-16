6966371c6bcbe297ccdd3a6c1c1928d4f16148ce
Belbouk Ayyoub <belboukayyoub@gmail.com>
2023-10-16 03:48:15 +0100

    0x00- Task 7. show

--- console.py
+++ console.py
@@ -32,6 +32,23 @@ class HBNBCommand(cmd.Cmd):
             b.save()
             print(b.id)
 
+    def do_show(self, arg):
+        """show: show [Class] [ID]"""
+        arg = arg.split()
+        data = storage.all()
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg[0] not in storage.classes():
+            print("** class doesn't exist **")
+        elif len(arg) == 1:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(arg[0], arg[1])
+            if key not in data:
+                print("** no instance found **")
+            else:
+                print(data[key])
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
